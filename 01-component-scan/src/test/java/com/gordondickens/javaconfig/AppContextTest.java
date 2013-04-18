7bba06f577e810f87de606d0f807bbd5a1382394
Gordon Dickens <gordon@gordondickens.com>
2013-04-18 17:44:11 -0400

    added readme updated dependencies

--- 01-component-scan/src/test/java/com/gordondickens/javaconfig/AppContextTest.java
+++ 01-component-scan/src/test/java/com/gordondickens/javaconfig/AppContextTest.java
@@ -5,11 +5,15 @@ import com.gordondickens.javaconfig.beans.Customer;
 import com.gordondickens.javaconfig.beans.Product;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.notNullValue;
 
 /**
@@ -18,7 +22,8 @@ import static org.hamcrest.Matchers.notNullValue;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = AppConfig.class)
 public class AppContextTest {
-    
+    private static final Logger logger = LoggerFactory.getLogger(AppContextTest.class);
+
     @Autowired
     Customer customer;
     
@@ -33,5 +38,10 @@ public class AppContextTest {
         assertThat(customer, notNullValue());
         assertThat(account, notNullValue());
         assertThat(product, notNullValue());
+        assertThat("Product Name MUST be at least 1 character", product.getProductName().length(), greaterThan(1));
+        assertThat("Product Name MUST match", product.getProductName(), equalToIgnoringCase("swizzles"));
+
+        logger.debug("Product {}", product.toString());
+
     }
 }
--- README.md
+++ README.md
@@ -2,3 +2,52 @@ spring-javaconfig-demos
 =======================
 
 Spring JavaConfig for Annotation based Spring Configuration
+
+
+General Features
+----------------
+* Demonstration of JavaConfig Annotation based Spring Configuration
+* Beans created in _src/main/java/com/gordondickens/javaconfig/AppContext.java_
+* Tests are in _src/test/java/com/gordondickens/javaconfig/AppContextText.java_
+* Logback with SLF4J for logging, see _src/test/resources/logback-test.xml_ for logging configuration
+
+
+01-Component-Scan
+-----------------
+* Demonstrates using _@ComponentScan_ in place of XML based _&lt;context:component-scan/&gt;_
+
+02-Enable-TX
+------------
+* Demonstrates using _@EnableTransactionManagement in place of XML based _&lt;tx:annotation-config/&gt;_
+
+
+
+03-Import-Resource
+------------------
+
+
+
+
+04-Import
+---------
+
+
+
+05-Property-Source
+------------------
+
+
+06-Nested-Configuration
+-----------------------
+* TODO - To be implemented
+
+
+07-Application-Initializer
+--------------------------
+* TODO - To be implemented
+
+
+
+
+
+
